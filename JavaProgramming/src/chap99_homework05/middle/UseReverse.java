a660650118b0f45574efbe5c4acbfde9da352b6d
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2024-05-03 18:55:39 +0900

    Add files via upload
    test

---
+++ JavaProgramming/src/chap99_homework05/middle/UseReverse.java
@@ -0,0 +1,13 @@
+package chap99_homework05.middle;
+
+public class UseReverse {
+
+	public static void main(String[] args) {
+		
+		Reverse reverse = new Reverse();
+
+		reverse.reverseString("text");
+		
+	}
+
+}
