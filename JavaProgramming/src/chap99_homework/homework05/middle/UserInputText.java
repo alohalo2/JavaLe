231c930453ddbe247c98e85c1e42190ff7caadc0
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2024-05-07 19:35:39 +0900

    Add files via upload
    test

---
+++ JavaProgramming/src/chap99_homework/homework05/middle/UserInputText.java
@@ -0,0 +1,33 @@
+package chap99_homework05.middle;
+
+import java.util.Scanner;
+
+public class UserInputText {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		// 2. 사용자가 입력한 문자열에서 중복된 문자를 제거한 문자열을 출력하세요.
+		// aabbccab
+		// abc
+		
+		String[] strArr = new String[strArr.length];
+		
+		String strArr[] = sc.nextLine();
+		
+		
+		
+		
+		
+		
+		
+		// 3. 사용자가 입력한 문자열에서 인덱스가 3의 배수 자리의 문자는 3으로 5의 배수 자리의 문자는 5로 변경하여 출력하세요.
+        // 3,5 공배수일때는 35로 변경
+		// aaaaaaaaaaa
+		// aaa3a53aa35
+	}
+
+}
