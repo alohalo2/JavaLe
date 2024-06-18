c2f05115d1294f4fcd9dd8d0f15f5962e9703593
alohalo2 <wntjd7892@naver.com>
2024-06-18 20:23:45 +0900

    24.06.18 이주성 update

--- JavaProgramming/src/chap99_homework/Main.java
+++ JavaProgramming/src/chap99_homework/Main.java
@@ -7,11 +7,26 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-	
+
+		int N = sc.nextInt(); // 정수의 갯수
+
+		int[] nArry = new int[N]; // 정수를 담는 배열
+
+		for (int i = 0; i < nArry.length; i++) {
+			nArry[i] = sc.nextInt();
+		}
+
+		int v = sc.nextInt(); // 찾으려고 하는 정수
+		int vCnt = 0;
+
+		for (int i = 0; i < nArry.length; i++) {
+			if (nArry[i] == v) {
+				vCnt++;
+			}
+		}
+
+		System.out.println(vCnt);
+
 		sc.close();
 	}
 }
-
-
-
