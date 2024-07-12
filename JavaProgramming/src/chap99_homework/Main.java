216bca9764a6b5250255fb34287ba465af58b761
alohalo2 <wntjd7892@naver.com>
2024-07-12 20:19:20 +0900

    24.07.12 update

--- JavaProgramming/src/chap99_homework/Main.java
+++ JavaProgramming/src/chap99_homework/Main.java
@@ -1,6 +1,5 @@
 package chap99_homework;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
@@ -8,21 +7,29 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		StringBuilder sb = new StringBuilder();
-		
+
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		
 		int[] intArr = new int[N];
-		
-		for(int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++) {
+			intArr[i] = i+1;
+		}
+		for (int i = 0; i < N; i++)
+		System.out.print(intArr[i] + " ");
+
+		for (int i = 0; i < M; i++) {
+
 			int I = sc.nextInt();
 			int J = sc.nextInt();
-			int K = sc.nextInt();
-			intArr[i] = K;
+
+			intArr[I - 1] = J;
+			intArr[J - 1] = I;
+			System.out.print(intArr[i] + " ");
+
 		}
-		for(int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			System.out.print(intArr[i] + " ");
 		}
 	}
