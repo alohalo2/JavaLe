a130430fc09e12cc51cbdbbf3debe78434a67928
alohalo2 <wntjd7892@naver.com>
2024-08-12 21:01:30 +0900

    24.08.12 update

--- JavaProgramming/src/chap99_homework/Main.java
+++ JavaProgramming/src/chap99_homework/Main.java
@@ -1,5 +1,6 @@
 package chap99_homework;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,29 +9,38 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int[] intArr = new int[N];
+		int[] noSubmitArr = new int[30];
+		int[] submitArr = new int[28];
+		int[] resultArr = new int[2];
+		int index = 0;
 
-		for (int i = 0; i < N; i++) {
-			intArr[i] = i+1;    
+			
+		for (int i = 0; i < 30; i++) {
+			noSubmitArr[i] = i+1;  
 		}
-		for (int i = 0; i < N; i++)
-		System.out.print(intArr[i] + " ");
-
-		for (int i = 0; i < M; i++) {
-
-			int I = sc.nextInt();
-			int J = sc.nextInt();   
-
-			intArr[I - 1] = J;
-			intArr[J - 1] = I;
-			System.out.print(intArr[i] + " ");
-
+		
+		for (int i = 0; i < 28; i++) {
+			submitArr[i] = sc.nextInt();
 		}
-
-		for (int i = 0; i < N; i++) {
-			System.out.print(intArr[i] + " ");
+		
+		for (int i = 0; i < noSubmitArr.length; i++) {
+		    boolean found = false;
+		    for (int j = 0; j < submitArr.length; j++) {
+		        if (noSubmitArr[i] == submitArr[j]) {
+		            found = true;
+		            break; // 같은 사람이 발견되면 내부 루프를 종료
+		        }
+		    }
+		    if (!found) { // noSubmitArr[i]가 submitArr에 존재하지 않으면
+		        resultArr[index] = noSubmitArr[i];
+		        index++; // resultArr의 다음 인덱스로 이동
+		    }
+		}
+		
+		Arrays.sort(resultArr);
+		
+		for (int i = 0; i < resultArr.length; i++) {
+		    System.out.println(resultArr[i]);  // 배열 요소를 출력
 		}
 	}
 }
