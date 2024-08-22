2cc4e647236eda1f9360763975447c6240f442e4
alohalo2 <wntjd7892@naver.com>
2024-08-22 20:05:05 +0900

    24.08.22 update

--- JavaProgramming/src/chap99_homework/Main.java
+++ JavaProgramming/src/chap99_homework/Main.java
@@ -1,29 +1,24 @@
 package chap99_homework;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {     
 
 		Scanner sc = new Scanner(System.in);
-   
+		int remain = 0;
+		Set<Integer> resultSet = new HashSet<>();
 		
-		
-		int N = sc.nextInt();
-		sc.nextLine();
-		int[] intArr = new int[N];
-		
-		for(int i = 0; i < intArr.length; i++) {
-			intArr[i] = sc.nextInt();
+		for(int i = 0; i < 10; i++) {
+			int N = sc.nextInt();
+			remain = N % 42;
+			resultSet.add(remain);
 		}
 		
-		Arrays.sort(intArr);
-		
-		for(int i : intArr) {
-			System.out.println(i);
-		}
+			System.out.println(resultSet.size());
 		
 		sc.close();
 	}
