d8bda48af4486b5b37ca01e7722681bb5a2ce1b8
alohalo2 <wntjd7892@naver.com>
2024-08-26 19:10:07 +0900

    24.08.26 update

--- JavaProgramming/src/chap99_homework/Main.java
+++ JavaProgramming/src/chap99_homework/Main.java
@@ -1,5 +1,7 @@
 package chap99_homework;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -9,16 +11,37 @@ public class Main {
 	public static void main(String[] args) {     
 
 		Scanner sc = new Scanner(System.in);
-		int remain = 0;
-		Set<Integer> resultSet = new HashSet<>();
 		
-		for(int i = 0; i < 10; i++) {
-			int N = sc.nextInt();
-			remain = N % 42;
-			resultSet.add(remain);
+		int N = sc.nextInt();
+		sc.nextLine();
+		int M = sc.nextInt();
+		
+		Integer[] intArr = new Integer[N];
+		
+		for(int i = 0; i < intArr.length; i++) {
+			intArr[i] = i+1;
 		}
 		
-			System.out.println(resultSet.size());
+		for(int i = 0; i < M; i++) {
+			int K = sc.nextInt();
+			int L = sc.nextInt();
+			
+//			for(int j = 1; j <  )
+			if(intArr[K] < intArr[L]) {
+				int temp = intArr[K];
+				intArr[K] = intArr[L - 1];
+				intArr[L - 1] = temp;
+				
+				Arrays.sort(intArr,Collections.reverseOrder());
+			}
+		
+		}
+		
+		Arrays.sort(intArr,Collections.reverseOrder());
+		
+		for(int i = 0; i < intArr.length; i++) {
+			System.out.print(intArr[i] + " ");
+		}
 		
 		sc.close();
 	}
