649487ffc552862a5027c59a363cf15432172634
alohalo2 <wntjd7892@naver.com>
2024-09-24 14:25:58 +0900

    24.09.24

--- JavaProgramming/src/chap99_homework/boj/_10156.java
+++ JavaProgramming/src/chap99_homework/boj/_10156.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _10156 {
 
 	public static void main(String[] args) {
 		
