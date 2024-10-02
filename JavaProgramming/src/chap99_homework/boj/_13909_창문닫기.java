1880cfbeea5686c3503b829c820fc9891a04939f
alohalo2 <wntjd7892@naver.com>
2024-10-02 11:09:40 +0900

    24.10.02

--- JavaProgramming/src/chap99_homework/boj/_13909_창문닫기.java
+++ JavaProgramming/src/chap99_homework/boj/_13909_창문닫기.java
@@ -5,7 +5,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class _13909_창문닫기 {
 
 public static void main(String[] args) {
 		
