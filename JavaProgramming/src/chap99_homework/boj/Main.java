e815b3e692cedc1973cf68773a412bf96ed37c45
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2025-03-24 17:48:46 +0900

    25.03.24

--- JavaProgramming/src/chap99_homework/boj/Main.java
+++ JavaProgramming/src/chap99_homework/boj/Main.java
@@ -5,32 +5,45 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	static int max = 0;
+	
 	public static void main(String[] args) {
 		
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-			
-			int N = Integer.parseInt(br.readLine());
-			int M = 0;
-			
-			for(int i = N - 9*String.valueOf(N).length(); i < N; i++) {
-				int sum = i;
-				int temp = i;
-				
-				while(temp > 0) {
-					sum += temp % 10;
-					temp /= 10;
-				}
-				
-				if(sum == N) {
-					M = i;
-					break;
-				}
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+			String[] userInput = br.readLine().split(" ");
+			int N = Integer.parseInt(userInput[0]);
+			int M = Integer.parseInt(userInput[1]);
+
+			int[] cardArry = new int[N];
+			String[] cards = br.readLine().split(" ");
+
+			for (int i = 0; i < cardArry.length; i++) {
+				cardArry[i] = Integer.parseInt(cards[i]);
 			}
-			
-			System.out.println(M);
+
+			combine(cardArry, new int[3], 0, 0, M);
+			System.out.println(max);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+
+	}
+	
+	public static void combine(int[] cardArry, int[] selected, int start, int depth, int M) {
+		
+		if(depth == 3) {
+			int result = selected[0] + selected[1] + selected[2];
+			if (result <= M && result > max) {
+				max = result;
+			}
+			return;
+		}
+		
+		for(int i = start; i < cardArry.length;	i++) {
+			selected[depth] = cardArry[i];
+			combine(cardArry, selected, i + 1, depth + 1, M);
 		}
 	}
 }
--- JavaProgramming/src/chap99_homework/boj/_2798_블랙잭.java
+++ JavaProgramming/src/chap99_homework/boj/_2798_블랙잭.java
@@ -2,37 +2,90 @@ package chap99_homework.boj;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class _2798_블랙잭 {
 
+	static int max = 0;
+
 	public static void main(String[] args) {
 
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-			
+		// gpt 코드
+		
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
 			String[] userInput = br.readLine().split(" ");
 			int N = Integer.parseInt(userInput[0]);
 			int M = Integer.parseInt(userInput[1]);
-			
-			
-			
-			int[] cardArry = new int[N];
-			String[] cards = br.readLine().split(" ");
-			
-			
-			for(int i = 0; i < cardArry.length; i++) {
-				cardArry[i] = Integer.parseInt(cards[i]);
-				System.out.println(cardArry[i]);
+
+			int[] cards = new int[N];
+			String[] inputs = br.readLine().split(" ");
+
+			for (int i = 0; i < N; i++) {
+				cards[i] = Integer.parseInt(inputs[i]);
 			}
-			
-			int sum = 0;
-			
-			
-			
-			
+
+			combine(cards, 0, 0, 0, M);
+			System.out.println(max);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
+
+	// selectedCount: 지금까지 고른 카드 수
+	// sum: 현재까지 합
+	public static void combine(int[] cards, int start, int selectedCount, int sum, int M) {
+		if (selectedCount == 3) {
+			if (sum <= M) {
+				max = Math.max(max, sum);
+			}
+			return;
+		}
+
+		for (int i = start; i < cards.length; i++) {
+			combine(cards, i + 1, selectedCount + 1, sum + cards[i], M);
+		}
+	}
+
+//		// 내 코드
+//		
+//		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+//
+//			String[] userInput = br.readLine().split(" ");
+//			int N = Integer.parseInt(userInput[0]);
+//			int M = Integer.parseInt(userInput[1]);
+//
+//			int[] cardArry = new int[N];
+//			String[] cards = br.readLine().split(" ");
+//
+//			for (int i = 0; i < cardArry.length; i++) {
+//				cardArry[i] = Integer.parseInt(cards[i]);
+//			}
+//
+//			combine(cardArry, new int[3], 0, 0, M);
+//			System.out.println(max);
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//	
+//	public static void combine(int[] cardArry, int[] selected, int start, int depth, int M) {
+//		
+//		if(depth == 3) {
+//			int result = selected[0] + selected[1] + selected[2];
+//			if (result <= M && result > max) {
+//				max = result;
+//			}
+//			return;
+//		}
+//		
+//		for(int i = start; i < cardArry.length;	i++) {
+//			selected[depth] = cardArry[i];
+//			combine(cardArry, selected, i + 1, depth + 1, M);
+//		}
+//	}
 
 }
