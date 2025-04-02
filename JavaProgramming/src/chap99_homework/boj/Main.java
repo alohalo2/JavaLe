753ffb9aefec21daf4b976b1674272a9eb41fad8
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2025-04-02 18:20:03 +0900

    25.04.02

--- JavaProgramming/src/chap99_homework/boj/Main.java
+++ JavaProgramming/src/chap99_homework/boj/Main.java
@@ -1,16 +1,40 @@
 package chap99_homework.boj;
 
 
-import java.nio.ByteOrder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-        if (ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN)) {
-            System.out.println("Big Endian");
-        } else {
-            System.out.println("Little Endian");
-        }
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+			int testCase = Integer.parseInt(br.readLine());
+
+			while (testCase != 0) {
+
+				int k = Integer.parseInt(br.readLine());
+				int n = Integer.parseInt(br.readLine());
+
+				int[][] peopleCount = new int[k + 1][n + 1];
+				
+				for(int i = 1; i <= n; i++) {
+					peopleCount[0][i] = i;
+				}
+
+				for (int i = 1; i < k + 1; i++) {
+					for (int j = 1; j <= n; j++) {
+						for(int m = 1; m <= j; m++) {
+							peopleCount[i][j] += peopleCount[i-1][m];
+						}
+					}
+				}
+				System.out.println(peopleCount[k][n]);
+				testCase--;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 }
--- JavaProgramming/src/chap99_homework/boj/class2/_2775_부녀회장이될테야.java
+++ JavaProgramming/src/chap99_homework/boj/class2/_2775_부녀회장이될테야.java
@@ -16,16 +16,22 @@ public class _2775_부녀회장이될테야 {
 				int k = Integer.parseInt(br.readLine());
 				int n = Integer.parseInt(br.readLine());
 
-				int[][] peopleCount = new int[k][n];
-				int count = 0;
+				int[][] peopleCount = new int[k + 1][n + 1];
+				
+				for(int i = 1; i <= n; i++) {
+					peopleCount[0][i] = i;
+				}
 
-				for (int i = 0; i < k; i++) {
-					for (int j = 0; j < n; j++) {
-						peopleCount[i][j] = count + j;
-						count += j;
-						System.out.println("peopleCount" + "[" + i + "]" + "[" + j + "]" + peopleCount[i][j]);
+				for (int i = 1; i < k + 1; i++) {
+					for (int j = 1; j <= n; j++) {
+						for(int m = 1; m <= j; m++) {
+							peopleCount[i][j] += peopleCount[i-1][m];
+						}
+//						System.out.printf("peopleCount[%d][%d]: %d%n",i,j,peopleCount[i][j]);
+//						System.out.println("peopleCount" + "[" + i + "]" + "[" + j + "]" + peopleCount[i][j]);
 					}
 				}
+				System.out.println(peopleCount[k][n]);
 				testCase--;
 			}
 		} catch (Exception e) {
