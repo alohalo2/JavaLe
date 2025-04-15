4e576cc0bfd85a3c367ae7e2a60d6c13892db55f
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2025-04-15 18:11:42 +0900

    25.04.15

--- JavaProgramming/src/chap99_homework/boj/class2/_7568_덩치.java
+++ JavaProgramming/src/chap99_homework/boj/class2/_7568_덩치.java
@@ -1,77 +1,76 @@
-package chap99_homework.boj;
+package chap99_homework.boj.class2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class _7568_덩치 {
 
 	public static void main(String[] args) {
-
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+			
 			StringBuilder sb = new StringBuilder();
-
+			
 			int N = Integer.parseInt(br.readLine());
-
+			
 			List<Big> bigs = new ArrayList<>();
-
-			for (int i = 0; i < N; i++) {
+			
+			for(int i = 0; i < N; i++) {
 				String[] str = br.readLine().split(" ");
 				int weight = Integer.parseInt(str[0]);
 				int height = Integer.parseInt(str[1]);
 				bigs.add(new Big(weight, height));
 			}
-
-			for (int i = 0; i < N; i++) {
+			
+			for(int i = 0; i < N; i++) {
 				int index = i;
 				sb.append(countGreaterPoints(bigs, index) + 1).append(" ");
 			}
-
+			
 			System.out.println(sb);
-
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		
 	}
-
+	
 	public static int countGreaterPoints(List<Big> bigs, int index) {
-		int count = 0;
-		Big base = bigs.get(index);
+	    int count = 0;
+	    Big base = bigs.get(index);
 
-		for (int i = 0; i < bigs.size(); i++) {
-			if (i == index)
-				continue;
+	    for (int i = 0; i < bigs.size(); i++) {
+            if (i == index) continue;
 
-			Big other = bigs.get(i);
+            Big other = bigs.get(i);
 
-			// base가 다른 점보다 크면 +1
-			if (base.compareToBig(other)) {
-				count++;
-			}
-		}
+            // base가 다른 점보다 크면 +1
+            if (base.compareToBig(other)) {
+                count++;
+            }
+	    }
 
-		return count;
+	    return count;
 	}
 
 }
 
 class Big {
-	int x, y;
-
+	int x,y;
+	
 	public Big(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-
-	public boolean compareToBig(Big other) {
-		if (this.x < other.x && this.y < other.y) {
-			return true;
-		} else {
+	
+    public boolean compareToBig(Big other) {
+        if (this.x < other.x && this.y < other.y) {
+        	return true;
+        } else {
 			return false;
 		}
-
-	}
+        
+    }
 }
